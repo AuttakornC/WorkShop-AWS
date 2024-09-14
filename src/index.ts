98b28b2e3b5ee9d13b4ce0b10f4759e1c8cd3be9
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-09-14 14:22:05 +0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req:Request, res:Response)=>{
-    res.send("index.html")
+    res.send("Hello World")
 })
 
 app.listen(5000, ()=>{console.log(`Server run on port:5000`)})
