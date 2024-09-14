ef034ae5c9541bc1a181260b01d59125bc6ca703
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-09-14 16:01:37 +0700

    Add auttakorn

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req:Request, res:Response)=>{
-    res.send("Hello World")
+    res.send("Hello World Auttakorn");
 })
 
 app.listen(5000, ()=>{console.log(`Server run on port:5000`)})
