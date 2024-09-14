70e6fdb95827cc90391dbcb55dfdb07f91678078
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-09-14 16:24:33 +0700

    Add second

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req:Request, res:Response)=>{
-    res.send("Hello World Auttakorn");
+    res.send("Hello World Auttakorn This is second na.");
 })
 
 app.listen(5000, ()=>{console.log(`Server run on port:5000`)})
